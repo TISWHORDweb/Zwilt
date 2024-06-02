39a32bf0704b1645e2ae95152657fc456e557405
Batimehin Emmanuel Oluwatimilehin <ebatimehin@gmail.com>
2024-06-02 20:35:53 +0100

    Adding sections to home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -5,6 +5,10 @@ import Section1 from './Section1'
 import Section2 from './Section2'
 import Section3 from './Section3'
 import Section4 from './Section4'
+import Section5 from './Section5'
+import Section6 from './Section6'
+import GetStarted from '../../Components/GetStarted'
+import Footer from '../../Components/Footer'
 
 function Home() {
   return (
@@ -15,6 +19,10 @@ function Home() {
       <Section2 />
       <Section3 />
       <Section4 />
+      <Section5 />
+      <Section6 />
+      <GetStarted />
+      <Footer />
     </div>
   )
 }
