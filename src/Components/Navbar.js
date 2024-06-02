593fb70d17aabac54268e936c1cbe0f4ed01fb5e
Batimehin Emmanuel Oluwatimilehin <ebatimehin@gmail.com>
2024-06-02 21:12:48 +0100

    Navbar media query update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,13 +6,13 @@ function Navbar() {
         <div>
             <div class="container">
                 <div className="Navbar mt-3">
-                    <nav class="navbar navbar-expand-lg ">
+                    <nav class="navbar navbar-expand-lg navbar-bg-dark ">
 
                         <span class="navbar-brand pt-0" href="#"><img src={Logo} alt="" /></span>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
+                        <span class="" role="button" ><i class="fa fa-bars" aria-hidden="true" style={{color:"#e6e6ff"}}></i></span>
                         </button>
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <div class="collapse navbar-collapse nav-list" id="navbarSupportedContent">
                             <ul class="navbar-nav m-auto mt-0 mb-2 mb-lg-0">
                                 <li class="nav-item">
                                     <span class="nav-link " aria-current="page" >Find Work</span>
@@ -30,9 +30,9 @@ function Navbar() {
                                     <span class="nav-link" href="#">Contact US</span>
                                 </li>
                             </ul>
-                            <div className="flex animate__animated animate__fadeInRight">
+                            <div className="flex">
                                 <span class="nav-link" >Log In</span>
-                                <button class="btn-white" type="submit">Join Now</button>
+                                <button class="btn-white flex animate__animated animate__fadeInRight" type="submit">Join Now</button>
                             </div>
                         </div>
 
